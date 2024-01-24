e0683859eb3f91377fad3f6aa22741e0636fce67
Jim Délitroz <jim.delitroz@hes-so.ch>
2024-01-24 13:44:17 +0100

    Improved the Baillieul' counterexample

--- Intro to PowerModels/Solutions and losses.jl
+++ Intro to PowerModels/Solutions and losses.jl
@@ -1,7 +1,7 @@
 using PowerModels, LinearAlgebra, StaticArrays, IntervalArithmetic, IntervalRootFinding, JuMP, Ipopt, ProgressBars
 using Colors, ForwardDiff, Dates, Plots
 
-include("C:/Users/jim.delitroz/Documents/Julia scripts/Intro to PowerModels/Solutions and losses-the functions.jl")
+include("C:/Users/jim.delitroz/Documents/Julia scripts/src/Intro to PowerModels/Solutions and losses-the functions.jl")
 
 function perform(V, T, B, G0, D, k = 0, i = 1)
     n = length(V)
@@ -9,55 +9,61 @@ function perform(V, T, B, G0, D, k = 0, i = 1)
     
     P = Powers(V, T, B, G)
 
-    #=
-    generated_power = 0
-    lost_power = 0
-
-
-    for p in [p1, p2, p3]
-        lost_power += p
-        if p > 0
-            generated_power += p
-        end
-    end
-
-    if generated_power == 0
-        rel_loss = 0
-    else
-        rel_loss = lost_power / generated_power
-    end 
-    println("Total loss _ $lost_power")
-    println("Relative loss : $rel_loss") =#
-    
     
     F = (x -> SVector{n-1}([f(x) for f in return_f_KS(V, B, P)]))
 
     rts = roots(F, D, Krawczyk, 1e-4)
 
-    display(rts)
+
     
+    numberofcolors = 38
     THD = false
-    if i > 15
-        i = 15
+    if i > numberofcolors
+        i = numberofcolors
     end
-    mycolors = cgrad([:tomato2, :green], 52, categorical = true)
+    mycolors = cgrad([:tomato2, :green], numberofcolors, categorical = true)
     if n == 3
         for r in rts
+            
             m = mid(r.interval)
-            if k > 0
-                plot!([m[1]], [m[2]], marker = :circle, color = mycolors[i], label="", show = true)
+            T2 = [[0]; m]
+            Pnow = Powers(V, T2, B, G)
+            total_loss = 0
+            for p in Pnow
+                total_loss += p
+            end
+            println("Total loss : $(round(total_loss, digits = 3))")
+            if total_loss < 0
+                println("$r Not physically meaningful")
             else
-                plot!([m[1]], [m[2]], marker = :circle, markersize = 5, color = :red, label="", show = true)
+                display(r)
+                if k > 0
+                    plot!([m[1]], [m[2]], marker = :circle, color = mycolors[i], label="", show = true)
+                else
+                    plot!([m[1]], [m[2]], marker = :circle, markersize = 5, color = :red, label="", show = true)
+                end
             end
 
         end
     elseif n == 4
         for r in rts
             m = mid(r.interval)
-            if k > 0
-                plot3d!([m[1]], [m[2]], [m[3]], marker = :circle, color = mycolors[i], label="", show = true)
+            T2 = [[0]; m]
+            Pnow = Powers(V, T2, B, G)
+            total_loss = 0
+            for p in Pnow
+                total_loss += p
+            end
+            println("Total loss : $total_loss")
+            if total_loss < 0
+                println("$r Not physically meaningful")
             else
-                plot3d!([m[1]], [m[2]], [m[3]], marker = :circle, color = :red, label="", show = true)
+                display(r)
+                if k > 0
+                    plot3d!([m[1]], [m[2]], [m[3]], marker = :circle, color = mycolors[i], label="", show = true)
+                else
+                    plot3d!([m[1]], [m[2]], [m[3]], marker = :circle, markersize = 5, color = :red, label="", show = true)
+                end
             end
         end
     else
@@ -99,7 +105,6 @@ function main()
     
    
 
-    D1 = (-pi..pi)
     mybox = []
     for i = 2:n
         push!(mybox, D1)
@@ -109,6 +114,7 @@ function main()
 
     println("k = 0")
     rts = perform(V, T, B, G0, D, 0)
+
     already_here = false
     i = 1
     L = collect(LinRange(0, 2, 50))
@@ -147,9 +153,9 @@ end
 
 function main_4D()
 
-    t2 = rand()*2*pi
-    t3 = rand()*2*pi
-    t4 = rand()*2*pi
+    t2 = 4.540823929597235
+    t3 = 4.952715884631364
+    t4 = 5.824477533260155
     println("Initial angles = $t2, $t3, $t4")
     T = [0., t2, t3, t4]
 
@@ -157,25 +163,40 @@ function main_4D()
     n = length(T)
 
     println("B susceptance matrix")
-    B = random_B(12, 4, true)
+    #B = random_B(12, 4, true)
+    B = [ -11.5312       10.4033     1.11996    0.00793174;
+    10.4033      -21.758      5.50708    5.84768;
+     1.11996       5.50708  -16.6064     9.97934;
+     0.00793174    5.84768    9.97934  -15.8349]
 
 
     println("G0 loss matrix")
-    G0 = random_B(8, 4, true)
+    #G0 = random_B(8, 4, true)
+    G0 = [-18.018      3.01572    7.80488    7.19738;
+    3.01572  -15.3365     5.13042    7.1904;
+    7.80488    5.13042  -18.1905     5.25517;
+    7.19738    7.1904     5.25517  -19.6429]
 
-    D1 = (-pi..pi)
-    mybox = []
+  
+    D2 = IntervalBox([interval(-1.47327, -1.47325), interval(-1.07745, -1.07743), interval(-0.20256, -0.202551)])
+    D1 = IntervalBox([interval(-1.74241, -1.74231), interval(-1.33051, -1.33043), interval(-0.458743, -0.458672)])
+
+    #=mybox = []
     for i = 2:n
         push!(mybox, D1)
     end
 
-    D = IntervalBox(mybox) 
+    D = IntervalBox(mybox) =#
 
     println("k = 0")
-    rts = perform(V, T, B, G0, D, 0)
+    rts = perform(V, T, B, G0, D1, 0)
+    println(typeof(rts))
+    rts2 = perform(V, T, B, G0, D2, 0)
+    rts = [rts; rts2]
+
     already_here = false
     i = 1
-    L = collect(LinRange(0.025, 0.3, 12))
+    L = collect(LinRange(0.025, 0.2, 25))
     for k in (L)
         println("k = $(round(k, digits = 3))")
         new_rts = []
@@ -210,4 +231,42 @@ function main_4D()
 
 end
 
-main_4D()
+function main_yielding_a_contradiction_bis() # 3 bus system that contradicts the assumption (losses increase -> solutions diminish)
+    #The accelerated method (ie the use of next_interval) supposes that the assumption is true, in particular, using it here will not yield a contradiction (although there is one)
+    t2 = 0
+    t3 = 3/2*pi
+    println("Initial angles = $t2, $t3")
+    T = [0., t2, t3]
+    V = [1., 1., 1.]
+    B = [-7 4 3; 4 -5 1; 3 1 -4]
+    println("B susceptance matrix")
+    display(B)
+    n = length(T)
+
+    G0 = [-8   5   3; # Contradicts the monotony of number of solutions with k : k=0 admits 2 sol; 0.2<=k<= admits 4
+    5  -7   2;
+    3   2  -5]   
+    
+    println("G0 loss matrix")
+    display(G0)
+
+    D1 = (-pi..pi)
+    mybox = []
+    for i = 2:n
+        push!(mybox, D1)
+    end
+
+    D = IntervalBox(mybox) 
+
+    i = 1
+    L = collect(LinRange(0, 2.5, 36))
+    for k in (L)
+        println("k = $k")
+        perform(V, T, B, G0, D, k, i)
+        i += 1
+    end
+    println("im done")
+    readline()
+end
+
+main_yielding_a_contradiction_bis()
